1bfbccb47a82b55b84f36e71d1e99a144e2c6e68
BigAru <arooran@indikon.ch>
2020-05-08 02:15:12 +0200

    add working curryprocessor

--- examples/src/main/java/Main.kt
+++ examples/src/main/java/Main.kt
@@ -3,10 +3,16 @@ import `in`.abaddon.tartarus.unholycurry.Curry
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        println("Meow ...")
-        noe()
+        val foo = Foo()
+        foo.uncurried("a",2,"c", false, 4.2)
+        foo.uncurried("a")(2)("c")(false)(4.2)
+    }
+}
+
+class Foo(){
+    @Curry
+    fun uncurried(a: String, b: Int, c: String, d: Boolean, e: Double){
+        println("->>> $a $b $c $d $e <<<-")
     }
 
-    @Curry
-    fun noe(){}
 }
--- unholycurry/src/main/java/in/abaddon/tartarus/unholycurry/CurryProcessor.kt
+++ unholycurry/src/main/java/in/abaddon/tartarus/unholycurry/CurryProcessor.kt
@@ -1,14 +1,11 @@
 package `in`.abaddon.tartarus.unholycurry
 
-import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.SupportedAnnotationTypes
-import javax.annotation.processing.SupportedOptions
-import javax.annotation.processing.SupportedSourceVersion
-import javax.annotation.processing.ProcessingEnvironment
-import javax.annotation.processing.RoundEnvironment
-import javax.annotation.processing.Messager
+import com.squareup.kotlinpoet.*
+import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
+import javax.lang.model.element.ExecutableElement
 import javax.lang.model.element.TypeElement
+import javax.lang.model.element.VariableElement
 import javax.tools.Diagnostic
 
 @SupportedAnnotationTypes("in.abaddon.tartarus.unholycurry.*")
@@ -26,10 +23,63 @@ class CurryProcessor: AbstractProcessor(){
         messager = processingEnv?.messager
     }
 
-    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
-        messager?.printMessage(Diagnostic.Kind.NOTE, "Wohoo....")
+    private fun log(msg: String){
+        processingEnv.messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, msg)
+    }
 
+    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        val methodBuffer = mutableListOf<ExecutableElement>()
+
+        annotations?.forEach { a -> run{
+            roundEnv?.getElementsAnnotatedWith(a)?.forEach { e -> run {
+                if(e is ExecutableElement) {
+                    methodBuffer.add(e)
+                }
+            }}
+        }}
+
+        if(methodBuffer.isNotEmpty()) printFiles(methodBuffer)
         return true
+    }
+
+    // workaround kotlin string
+    fun TypeName.correctStringType() =
+        if (this.toString() == "java.lang.String") ClassName("kotlin", "String") else this
+
+
+    private fun makeParam(p: VariableElement): ParameterSpec{
+        return ParameterSpec.builder(p.simpleName.toString(), p.asType().asTypeName().correctStringType()).build()
+    }
+
+    private fun printFiles(methodBuffer: List<ExecutableElement>){
+        val method = methodBuffer.first()
+        val classElement = method.enclosingElement as TypeElement
+        log(""+ classElement.kind)
+
+        val firstParam = method.parameters.first()
+        val tailParam = method.parameters.drop(1).reversed()
+
+        val finalType = tailParam.map{makeParam(it)}
+                                 .fold(method.returnType.asTypeName()) {acc, p -> LambdaTypeName.get(returnType = acc, parameters = listOf(p))}
+
+        val args = method.parameters.map{it.simpleName.toString()}
+                                    .reduce{acc, a -> "$acc, $a"}
+
+        val body = tailParam.map{it.simpleName.toString()}
+                            .fold("this.${method.simpleName}($args)") {acc, s -> "{$s -> $acc}"}
+
+        val curriedFn = FunSpec.builder(method.simpleName.toString())
+                .receiver(classElement.asType().asTypeName())
+                .addParameter(makeParam(firstParam))
+                .returns(finalType)
+                .addStatement("return $body")
+                .build()
+
+        val file = FileSpec.builder("", "CurriedExtensions")
+                           .addFunction(curriedFn)
+                           .build()
+
+        file.writeTo(processingEnv.filer)
     }
 
 }
